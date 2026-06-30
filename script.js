1f52922b8c7a0c050ed6a983afaacf1996227aaf
Kauã Vinicius <costa.rosa.kaua@escola.pr.gov.br>
2026-06-30 12:25:34 +0000

    ok

--- script.js
+++ script.js
@@ -45,6 +45,7 @@ const perguntas = [
 
 let atual = 0;
 let perguntaAtual;
+let historiaFinal = "";
 
 function mostraPergunta() {
   perguntaAtual = perguntas[atual];
@@ -55,9 +56,17 @@ function mostraAlternativas() {
   for (const alternativa of perguntaAtual.alternativas) {
     const botaoAlternativas = document.createElement("button");
     botaoAlternativas.textContent = alternativa;
+    botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
     caixaAlternativas.appendChild(botaoAlternativas);
   }
 }
+
+function respostaSelecionada(opcaoSelecionada){
+  const afirmacoes = opcaoSelecionada.afirmacoes;
+  historiaFinal = afirmacoes;
+    atual++;
+    mostraPergunta();
+  }
 
 mostraPergunta();
 
